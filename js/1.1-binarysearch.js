023b070490b781ecb84770f5f84bd8dc42ea9ed4
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-07-11 00:34:30 -0700

    random isPalindrome and a solution that blew my mind..

--- js/1.1-binarysearch.js
+++ js/1.1-binarysearch.js
@@ -13,7 +13,25 @@ var twoSum = function (nums, target) {
 // console.log(twoSum([2, 7, 11, 15], 18))
 
 
+var isPalindrome = function (x) {
+    if (x < 0) return false;
+    const arr = x.toString().split("");
+    const reversed = arr.reverse().join("");
+    if (x === parseInt(reversed)) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
+//blew my mind solution
+var isPalindrome = function (x) {
+    if (x < 0) return false;
+
+    let rev = 0;
+    for (let i = x; i >= 1; i = Math.floor(i / 10)) rev = rev * 10 + i % 10
+    return rev === x;
+};
 
 
 
