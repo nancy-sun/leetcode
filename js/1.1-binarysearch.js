ddf20d582ac7216016fb8e13ff24c77c3dcd9698
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-07-12 23:44:02 -0700

    review

--- js/1.1-binarysearch.js
+++ js/1.1-binarysearch.js
@@ -174,7 +174,6 @@ function mySqrt(x) {
     return right;
 }
 
-
 /* 268. Missing Number
 Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 */
