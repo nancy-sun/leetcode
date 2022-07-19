3d0d17c35a4b03838d291940ea89245e70a93ef3
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-07-18 22:21:29 -0700

    review strings, learning linked list

---
+++ js/ds-1-linkedlist.js
@@ -0,0 +1,24 @@
+/* ------------------------------------------DAY 7------------------------------------------*/
+/* 141. First Unique Character in a String
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. 
+
+Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+*/
+
+function hasCycle(head) {
+    //start from the head node
+    let fast = head;
+    let slow = head;
+
+    while (fast && fast.next) { //when the next value is not null, means it links to a node, aka has cycle 
+        fast = fast.next.next; //fast moves two steps
+        slow = slow.next; //slow moves one step
+        if (fast == slow) return true; //when fast meets with slow, means it's connected aka cycle
+    }
+    return false; //if they do not meet no cycle
+}
+
+console.log(hasCycle([3, 2, 0, -4]))
--- js/udemys.js
+++ js/udemys.js
@@ -179,3 +179,39 @@ function findLongestSubstring(str) {
 
 // console.log(findLongestSubstring("qqqqqqqqqq"))
 // console.log(findLongestSubstring("zzxcz"))
+
+
+
+
+
+/* linked list */
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+let list = new LinkedList;
+list.push("hello")
+list.push("world")
+console.log(list)
