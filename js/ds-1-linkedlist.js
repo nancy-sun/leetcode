0da828dd8bd5567f1b02f89c63b1c899869faf5c
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-07-20 23:56:50 -0700

    still learning linked list T_T

--- js/ds-1-linkedlist.js
+++ js/ds-1-linkedlist.js
@@ -31,4 +31,46 @@ function hasCycle2(head) {
     return false; //when we get to the point of null, means we reach the tail of the linked list and we are never get back to the seen point, aka no cycle
 }
 
-console.log(hasCycle2([3, 2, 0, -4]))
+
+
+/* 21. Merge Two Sorted Lists
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+*/
+
+//recursion
+function mergeTwoLists(list1, list2) {
+    if (!list1) { //when reaching the end of node in list1
+        return list2;
+    } else if (!list2) { //when reaching the end of node in list2
+        return list1;
+    } else if (list1.val < list2.val) { //if list 1 head < list 2 head, take list1's head
+        list1.next = mergeTwoLists(list1.next, list2); //list1.next is the list of unsorted list
+        return list1; //list 1 now only has the sorted nodes
+    } else { //take list2's head
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2; //list 2 now only has the sorted nodes
+    } //running recursively, passing in the sorted and the unsorted
+}
+
+//iteration
+function mergeTwoLists2(list1, list2) {
+    const head = new ListNode(); //storing result list
+    let node = head;
+    while (list1 && list2) { //if neither of the list gets to the end, take the smaller node, iterate
+        if (list1.val > list2.val) {
+            node.next = list2;
+            list2 = list2.next;
+        } else {
+            node.next = list1;
+            list1 = list1.next;
+        }
+        node = node.next; //move to the next node
+    } //merging until one list is empty
+    node.next = list1 || list2; //when one of the list gets to the end, attach the rest of the other list to it
+    return head.next; //return the entire list with attached list.
+}
+
+console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]));
